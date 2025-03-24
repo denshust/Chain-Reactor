2ab060d163aa67a6accf4aeef42af4c1d856c391
denshust <denshust@ukr.net>
2025-03-24 19:27:24 +0100

    Update SMILE.cs

--- Assets/Scripts/SMILE.cs
+++ Assets/Scripts/SMILE.cs
@@ -4,18 +4,5 @@ using UnityEngine;
 
 public class SMILE : MonoBehaviour
 {
-    public float Health;
-    public float TEst111;
-    // Start is called before the first frame update
-
-    // Update is called once per frame
-    void Update()
-    {
-        Testing2();
-    }
-
-    void Testing2()
-    {
-        int t2 = 2;
-    }
+    
 }
