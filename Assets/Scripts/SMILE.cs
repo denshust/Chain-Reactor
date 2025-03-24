b9ffcb6bfcae07d77615e2ccfeab1457c66295ed
denshust <denshust@ukr.net>
2025-03-24 19:38:37 +0100

    t

--- Assets/Scripts/SMILE.cs
+++ Assets/Scripts/SMILE.cs
@@ -6,11 +6,16 @@ public class SMILE : MonoBehaviour
 {
     public float Health;
     public float TEst111;
+    public int rrr;
     // Start is called before the first frame update
-
+    private void Start()
+    {
+        rrr = 12;
+    }
     // Update is called once per frame
     void Update()
     {
+        rrr = 2;
         Testing2();
     }
 
